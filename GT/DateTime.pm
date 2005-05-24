3ee1128012c746d7d5fd92056a67e98175902041
joao <joao@bb1899a8-85f5-0310-a327-fe9f4e15340a>
2005-05-24 23:23:04 +0000

    Intraday Timeframes for the DateTime object
    git-svn-id: https://devel.geniustrader.org:8082/svn/trunk@476 bb1899a8-85f5-0310-a327-fe9f4e15340a

--- GT/DateTime.pm
+++ GT/DateTime.pm
@@ -5,22 +5,23 @@ package GT::DateTime;
 # version 2 or (at your option) any later version.
 
 use strict;
-use vars qw(@ISA @EXPORT $PERIOD_1MIN $PERIOD_2MIN $PERIOD_5MIN $PERIOD_10MIN
-	    $PERIOD_30MIN $HOUR $DAY $WEEK $MONTH $YEAR %NAMES);
+use vars qw(@ISA @EXPORT $PERIOD_1MIN $PERIOD_5MIN $PERIOD_10MIN
+	    $PERIOD_15MIN $PERIOD_30MIN $HOUR $PERIOD_3HOUR $DAY $WEEK $MONTH $YEAR %NAMES);
 
 require Exporter;
 @ISA = qw(Exporter);
-@EXPORT = qw($PERIOD_1MIN $PERIOD_2MIN $PERIOD_5MIN $PERIOD_10MIN
-	     $PERIOD_30MIN $HOUR $DAY $WEEK $MONTH $YEAR);
+@EXPORT = qw($PERIOD_1MIN $PERIOD_5MIN $PERIOD_10MIN
+	     $PERIOD_15MIN $PERIOD_30MIN $HOUR $PERIOD_3HOUR $DAY $WEEK $MONTH $YEAR);
 
 #ALL#  use Log::Log4perl qw(:easy);
 
 $PERIOD_1MIN = 10;
-$PERIOD_2MIN = 20;
 $PERIOD_5MIN = 30;
 $PERIOD_10MIN = 40;
+$PERIOD_15MIN = 45;
 $PERIOD_30MIN = 50;
 $HOUR = 60;
+$PERIOD_3HOUR = 64;
 $DAY = 70;
 $WEEK = 80;
 $MONTH = 90;
@@ -28,17 +29,25 @@ $YEAR = 100;
 
 %NAMES = (
     $PERIOD_1MIN => "1min",
-    $PERIOD_2MIN => "2min",
     $PERIOD_5MIN => "5min",
     $PERIOD_10MIN => "10min",
+    $PERIOD_15MIN => "15min",
     $PERIOD_30MIN => "30min",
     $HOUR => "hour",
+    $PERIOD_3HOUR => "3hour",
     $DAY => "day",
     $WEEK => "week",
     $MONTH => "month",
     $YEAR => "year"
 );
 
+require GT::DateTime::1Min;
+require GT::DateTime::5Min;
+require GT::DateTime::10Min;
+require GT::DateTime::15Min;
+require GT::DateTime::30Min;
+require GT::DateTime::Hour;
+require GT::DateTime::3Hour;
 require GT::DateTime::Day;
 require GT::DateTime::Week;
 require GT::DateTime::Month;
@@ -51,8 +60,8 @@ GT::DateTime - Manage TimeFrames and provides date/time helper functions
 =head1 DESCRIPTION
 
 This module exports all the variable describing the available "periods"
-commonly used for trading : $PERIOD_1MIN, $PERIOD_2MIN, $PERIOD_5MIN,
-$PERIOD_10MIN, $PERIOD_30MIN, $HOUR, $DAY, $WEEK, $MONTH, $YEAR.
+commonly used for trading : $PERIOD_1MIN, $PERIOD_5MIN,
+$PERIOD_10MIN, $PERIOD_15MIN, $PERIOD_30MIN, $HOUR, $PERIOD_3HOUR, $DAY, $WEEK, $MONTH, $YEAR.
 
 The timeframes are represented by those variables which are only numbers.
 You can compare those numbers to know which timeframe is smaller or which
@@ -85,7 +94,14 @@ versa.
 =cut
 sub map_date_to_time {
     my ($timeframe, $date) = @_;
-    
+
+    $timeframe == $PERIOD_1MIN && return GT::DateTime::1Min::map_date_to_time($date);
+    $timeframe == $PERIOD_5MIN && return GT::DateTime::5Min::map_date_to_time($date);
+    $timeframe == $PERIOD_10MIN && return GT::DateTime::10Min::map_date_to_time($date);
+    $timeframe == $PERIOD_15MIN && return GT::DateTime::15Min::map_date_to_time($date);
+    $timeframe == $PERIOD_30MIN && return GT::DateTime::30Min::map_date_to_time($date);
+    $timeframe == $HOUR  && return GT::DateTime::Hour::map_date_to_time($date);
+    $timeframe == $PERIOD_3HOUR  && return GT::DateTime::3Hour::map_date_to_time($date);
     $timeframe == $DAY   && return GT::DateTime::Day::map_date_to_time($date);
     $timeframe == $WEEK  && return GT::DateTime::Week::map_date_to_time($date);
     $timeframe == $MONTH && return GT::DateTime::Month::map_date_to_time($date);
@@ -94,7 +110,14 @@ sub map_date_to_time {
 
 sub map_time_to_date {
     my ($timeframe, $time) = @_;
-    
+
+    $timeframe == $PERIOD_1MIN && return GT::DateTime::1Min::map_time_to_date($time);
+    $timeframe == $PERIOD_5MIN && return GT::DateTime::5Min::map_time_to_date($time);
+    $timeframe == $PERIOD_10MIN && return GT::DateTime::10Min::map_time_to_date($time);
+    $timeframe == $PERIOD_15MIN && return GT::DateTime::15Min::map_time_to_date($time);
+    $timeframe == $PERIOD_30MIN && return GT::DateTime::30Min::map_time_to_date($time);
+    $timeframe == $HOUR  && return GT::DateTime::Hour::map_time_to_date($time);
+    $timeframe == $PERIOD_3HOUR  && return GT::DateTime::3Hour::map_time_to_date($time);
     $timeframe == $DAY   && return GT::DateTime::Day::map_time_to_date($time);
     $timeframe == $WEEK  && return GT::DateTime::Week::map_time_to_date($time);
     $timeframe == $MONTH && return GT::DateTime::Month::map_time_to_date($time);
@@ -121,8 +144,8 @@ Returns the list of timeframes that are managed by the DateTime framework.
 =cut
 sub list_of_timeframe {
     return (
-	    #$PERIOD_1MIN, $PERIOD_2MIN, $PERIOD_5MIN, $PERIOD_10MIN,
-	    #$PERIOD_30MIN, $HOUR
+	    $PERIOD_1MIN, $PERIOD_5MIN, $PERIOD_10MIN,
+	    $PERIOD_15MIN, $PERIOD_30MIN, $HOUR, $PERIOD_3HOUR,
 	    $DAY, $WEEK, $MONTH, $YEAR
 	   );
 }
@@ -166,6 +189,13 @@ sub timeframe_ratio {
     {
 	return (1 / timeframe_ratio($second, $first));
     }
+    $first == $PERIOD_1MIN && return (GT::DateTime::Hour::timeframe_ratio($second) / 60);
+    $first == $PERIOD_5MIN && return (GT::DateTime::Hour::timeframe_ratio($second) / 12);
+    $first == $PERIOD_10MIN && return (GT::DateTime::Hour::timeframe_ratio($second) / 6);
+    $first == $PERIOD_15MIN && return (GT::DateTime::Hour::timeframe_ratio($second) / 4);
+    $first == $PERIOD_30MIN && return (GT::DateTime::Hour::timeframe_ratio($second) / 2);
+    $first == $HOUR && return GT::DateTime::Hour::timeframe_ratio($second);
+    $first == $PERIOD_3HOUR && return (GT::DateTime::Hour::timeframe_ratio($second) * 3);
     $first == $DAY && return GT::DateTime::Day::timeframe_ratio($second);
     $first == $WEEK && return GT::DateTime::Week::timeframe_ratio($second);
     $first == $MONTH && return GT::DateTime::Month::timeframe_ratio($second);
