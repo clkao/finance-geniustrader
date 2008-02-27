eba0c521ad7cfc610ee93473d318b6308f38f348
ras <ras@bb1899a8-85f5-0310-a327-fe9f4e15340a>
2008-02-27 03:25:37 +0000

    incorporated configuration item header_lines -- count of lines to skip minor tweaks to pod in general
    git-svn-id: https://devel.geniustrader.org:8082/svn/trunk@557 bb1899a8-85f5-0310-a327-fe9f4e15340a

--- GT/DB/Text.pm
+++ GT/DB/Text.pm
@@ -4,8 +4,8 @@ package GT::DB::Text;
 # This file is distributed under the terms of the General Public License
 # version 2 or (at your option) any later version.
 
-# includes fix for timeframes > day
 # new version joao costa circa nov 07
+# $Id$
 
 use strict;
 our @ISA = qw(GT::DB);
@@ -25,44 +25,55 @@ text files.
 
 =head2 Configuration
 
-All configuration items are required
+Most configuration items have default values, to alter these defaults
+you must indicate the configuration item and its value in your
+$HOME/.gt/options file.
 
-DB::module Text
+DB::module	Text -- informs gt you are using the Text.pm module. This
+configuration item is always required in your $HOME/.gt/options file.
 
-DB::text::directory  path where files are stored
+DB::text::directory	path -- where files are stored. This
+configuration item is always required in your $HOME/.gt/options file.
 
-DB::text::marker  string which delimits fields in each row of the data file
+DB::text::marker	string -- which delimits fields in each row of the data file.
+The marker defaults to the tab character '\t'.
 
-DB::text::file_extension  string to be appended to the code file name when 
-searching the data file.  for instance, if the data file is called EURUSD.csv
-this variable would have the value .csv
+DB::Text::header_lines	number -- The number of header lines in your data file
+that are to be skipped during processing. Lines with the either the
+comment symbol '#' or the less than symbol '<' as the first character
+do not need to be included in this value.. The header_lines default value is 0.
+
+DB::text::file_extension	string -- to be appended to the code file name when 
+searching the data file.  For instance, if the data file is called EURUSD.csv
+this variable would have the value '.csv' (without the quotes).
+
+The default file_extension is '.txt'.
 
 if you have data in different timeframes, for instance, EURUSD_hour.csv and
 EURUSD_day.csv, use the following value for this directive:
 
-DB::text::file_extension  _$timeframe.csv
+DB::text::file_extension	_$timeframe.csv
 
-DB::text::fields::datetime  Column index where to find the period datetime field.
-Indexes are 0 based.  For the particular case of datetime, can contain
+DB::text::fields::datetime	number -- Column index where to find the period datetime
+field. Indexes are 0 based.  For the particular case of datetime, can contain
 multiple indexes, useful when date and time are separate columns in the data
 file.  The date time format is anything that can be understood by Date::Manip.
-A typical example would be YYYY-MM-DD HH:NN:SS
+A typical example would be YYYY-MM-DD HH:NN:SS. The default datetime index is 5.
 
+DB::text::fields::open	number -- Column index where to find the period open field.
+Indexes are 0 based. The default open index is 0.
 
-DB::text::fields::open Column index where to find the period open field.
-Indexes are 0 based.  
+DB::text::fields::low	number -- Column index where to find the period low field.
+Indexes are 0 based. The default low index is 2. 
 
-DB::text::fields::low Column index where to find the period low field.
-Indexes are 0 based.  
+DB::text::fields::high	number -- Column index where to find the period high field.
+Indexes are 0 based. The default high index is 1.
 
-DB::text::fields::high Column index where to find the period high field.
-Indexes are 0 based.  
+DB::text::fields::close	number -- Column index where to find the period close field.
+Indexes are 0 based. The default close index is 3.
 
-DB::text::fields::close Column index where to find the period close field.
-Indexes are 0 based.  
-
-DB::text::fields::volume Column index where to find the period volume field.
-Indexes are 0 based.  
+DB::text::fields::volume	number -- Column index where to find the period volume field.
+Indexes are 0 based. The default volume index is 4.
 
 
 =head2 new()
@@ -77,6 +88,7 @@ sub new {
 
     my $self = { "directory" => GT::Conf::get("DB::Text::directory")};
 
+    GT::Conf::default('DB::Text::header_lines', '0');
     GT::Conf::default('DB::Text::marker', "\t");
     GT::Conf::default('DB::Text::file_extension', '.txt');
     GT::Conf::default('DB::Text::fields::datetime', '5');
@@ -86,6 +98,7 @@ sub new {
     GT::Conf::default('DB::Text::fields::close', '3');
     GT::Conf::default('DB::Text::fields::volume', '4');
 
+    $self->{'header_lines'} = GT::Conf::get('DB::Text::header_lines');
     $self->{'mark'} = GT::Conf::get('DB::Text::marker');
     $self->{'extension'} = GT::Conf::get('DB::Text::file_extension');
     $self->{'datetime'} = GT::Conf::get('DB::Text::fields::datetime');
@@ -144,13 +157,13 @@ sub get_prices {
     my $file = $self->{'directory'} . "/$code" . $extension;
 
     #open(FILE, "<$file") || (warn "Can't open $file: $!\n" and return GT::Prices->new());
-    open(FILE, "<", "$file")
-     || (warn "Can't open $file: $!\n" and return GT::Prices->new());
+    open(FILE, "<", "$file") || (warn "Can't open $file: $!\n" and return GT::Prices->new());
 
     my ($open, $high, $low, $close, $volume, $date);
     my ($year, $month, $day);
 
-
+    my $lines_to_skip = $self->{'header_lines'};
+    
     #TODO
     #Date::Manip requires this to be defined
     #there probably is a better way of doing this
@@ -161,6 +174,12 @@ sub get_prices {
     # Process each line in $file...
     while (defined($_=<FILE>))
     {
+        # Skip user specified number of file header lines
+        if ( $lines_to_skip > 0 ) {
+            $lines_to_skip--;
+            next;
+        }
+        
         next if (/^[#<]/); #Skip comments and METASTOCK ascii file header
         # Get and split the line with $mark
         chomp;
@@ -174,7 +193,7 @@ sub get_prices {
         $volume = $line[$self->{'volume'}] or $volume = 0; #some datasets don't include volume
         my $datetime=$line[$datetime_fields[0]];
         for (my $i=1; $i<$datetime_fields_count;$i++) {
-            $datetime .= ' '.$line[$datetime_fields[$i]];
+             $datetime .= ' '.$line[$datetime_fields[$i]];
         }
         $date = &UnixDate($datetime, '%Y-%m-%d %H:%M:%S');
 
