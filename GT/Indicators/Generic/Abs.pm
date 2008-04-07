93a705ba52d1a7c5fb9e8094ddb8d4f858edc8fe
joao <joao@bb1899a8-85f5-0310-a327-fe9f4e15340a>
2008-04-07 23:48:45 +0000

    Generic indicator, simply returns the absolute value of its first argument.
    git-svn-id: https://devel.geniustrader.org:8082/svn/trunk@598 bb1899a8-85f5-0310-a327-fe9f4e15340a

---
+++ GT/Indicators/Generic/Abs.pm
@@ -0,0 +1,40 @@
+package GT::Indicators::Generic::Abs;
+
+# Copyright 2008 João Antunes Costa
+# This file is distributed under the terms of the General Public License
+# version 2 or (at your option) any later version.
+
+# Standards-Version: 1.0
+
+use strict;
+use vars qw(@ISA @NAMES);
+
+use GT::Indicators;
+@ISA = qw(GT::Indicators);
+
+@NAMES = ("Abs[#1]");
+
+=head1 NAME
+
+GT::Indicators::Generic::Abs - Return the absolute value of its 1st parameter
+
+=head1 DESCRIPTION
+
+
+
+=cut
+sub initialize {
+    my ($self) = @_;
+}
+
+sub calculate {
+	my ($self, $calc, $i) = @_;
+	my $name = $self->get_name;
+
+	return if ($calc->indicators->is_available($name, $i));
+
+	my $val = $self->{'args'}->get_arg_values($calc, $i, 1);
+	return unless (defined($val));
+	my $res = abs($val);
+	$calc->indicators->set($name, $i, $res);
+}
