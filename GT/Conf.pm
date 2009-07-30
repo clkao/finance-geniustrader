e5763a32685f702f720a7708d2dfb3441ffdf3df
ras <ras@bb1899a8-85f5-0310-a327-fe9f4e15340a>
2009-07-30 05:18:51 +0000

    update continution/comment line processing
    git-svn-id: https://devel.geniustrader.org:8082/svn/trunk@690 bb1899a8-85f5-0310-a327-fe9f4e15340a

--- GT/Conf.pm
+++ GT/Conf.pm
@@ -1,11 +1,10 @@
 package GT::Conf;
 
 # Copyright 2000-2002 Raphaël Hertzog, Fabien Fulhaber
+# enhancements by ras copyright 2007-2008
 # This file is distributed under the terms of the General Public License
 # version 2 or (at your option) any later version.
 
-# baseline: 24 Apr 2005 2370 bytes
-# revised based on update 01jan07
 # $Id$
 
 use strict;
@@ -35,27 +34,39 @@ blank lines and lines with only whitespace are ignored.
 
 =head1 EXAMPLES of ~/.gt/options Entries
 
-# this is an example of a comment
+ # this is an example of a comment
 
-DB::module genericdbi
+ DB::module genericdbi
 
-DB::bean::dbname beancounter
+ DB::bean::dbname beancounter
 
-Graphic::Candle::UpBorderColor "[0,180,80]"
+ Graphic::Candle::UpBorderColor "[0,180,80]"
 
-Graphic::Candle::DownBorderColor "[180,0,80]"
+ Graphic::Candle::DownBorderColor "[180,0,80]"
 
 this example shows how continuing key values across lines can be useful.
 
-DB::genericdbi::prices_sql SELECT day_open, day_high, day_low, \
-  day_close, volume, date FROM stockprices WHERE symbol = '$code' ORDER \
-  BY date DESC
+ DB::genericdbi::prices_sql SELECT day_open, day_high, day_low, \
+   day_close, volume, date FROM stockprices WHERE symbol = '$code' ORDER \
+   BY date DESC
 
-the following shows why comments are not permitted on data lines:
+comments are permitted on data lines provided they can be distinguished from
+positional arguments markers (e.g. #1, #2, etc). in order to do this any
+trailing data line comment marker (#) must be surrounded by whitespace.
+the code is a bit more forgiving, using this regex (\s+#[\s\D]+.$)
 
-Aliases::Global::TFS2[] SY:TFS #1 #2 | CS:SY:TFS #1
+note that the comment must follow the end of the logical data line
+and terminates at the end of the logical line. logical line means the
+line after continuation processing has completed.
 
-=over
+examples:
+ 
+  Aliases::Global::TFS2[]	SY:TFS #1 #2 | CS:SY:TFS #1 # comment
+  graphic::positions::buycolor	"[0,135,0]" # very dark green
+  graphic::buysellarrows::buycolor	"[0,135,0,64]" # semitransparent dark green
+
+ note: configuration keys are lower cased automatically regardless of how
+       they are defined, but their values are as specified when defined
 
 =head1 FUNCTIONS
 
@@ -65,27 +76,22 @@ Load the configuration from the indicated file. If the file is omitted
 then it looks at ~/.gt/options by default.
 
 =cut
+
 sub load {
     my ($file) = @_;
     $file = _get_home_path() . "/.gt/options" if (! defined($file));
 
     warn ("Could not find configuration file: $file") and return if (! -e $file);
 
-    # changed from "< $file" per pg 625 programming perl 3rd ed.
     open (FILE, "<", $file) || die "Can't open $file: $!\n";
-    my $buf = '';
     while (<FILE>)
     {
 	chomp;
 
         # ras hack -- allow multi-line values in gt options file
-        if ( /\\$/ ) {
-          s/\\//;             # remove \
-          $buf .= $_;         # save line
-          next;               # get next line
-        } else {
-          $_ = $buf . $_;     # collect complete line into $_
-          $buf = '';          # reset line buffer
+        if ( s/\\$// ) {        # detect and remove continuation \
+            $_ .= <FILE>;           # concatenate to prior part
+            redo unless eof;    # don't read past file eof
         }
 
 	next if /^\s*#/;      # remove lines containing whitespace and/or comments
@@ -93,6 +99,12 @@ sub load {
 	s/^\s*//; s/\s*$//;   # remove leading and trailing whitespace
 
 	tr/[ \t]/[ \t]/s;     # squeeze out multiple adjacent whitespaces
+
+        # remove trailing comment
+        # in order to distinguish between a comment and a positional argument
+        # marker (e.g. #1, #2, etc) the comment marker must be followed by
+        # either whitespace or a non-digit (\s+#[\s\D]+.$)
+        s/(\s+#[\s\D]+.*)$//;
 
 	my ($key, $val) = split /\s+/, $_, 2;
 	$conf{lc($key)} = $val;
@@ -127,6 +139,7 @@ sub load {
 Clear all the configuration.
 
 =cut
+
 sub clear { %conf = () }
 
 =item C<< GT::Conf::store($file) >>
@@ -135,11 +148,11 @@ Write all the current configuration in the given file. Note: all prior
 commentary, if any is lost.
 
 =cut
+
 sub store {
     my ($file) = @_;
     $file = _get_home_path() . "/.gt/options" if (! defined($file));
 
-    # changed from "> $file" per pg 625 programming perl 3rd ed.
     open (FILE, ">", $file) || die "Can't write on $file: $!\n";
     foreach (sort keys %conf)
     {
@@ -156,6 +169,7 @@ key doesn't exist, it returns the optional defaultValue.
 If neither the key nor defaultValue exist, it returns undef.
 
 =cut
+
 sub get { 
     my $value = $conf{lc($_[0])};
     return $value if (defined($value));
@@ -169,6 +183,7 @@ Set the given configuration item to the corresponding value. Replaces any
 previous value.
 
 =cut
+
 sub set { $conf{lc($_[0])} = $_[1] }
 
 =item C<< GT::Conf::default($key, $value) >>
@@ -177,6 +192,7 @@ Set a default value to the given item. Must be called by GT itself to
 give reasonable default values to most of configurations items.
 
 =cut
+
 sub default {
     my ($key, $val) = @_;
     $key = lc($key);
@@ -191,6 +207,7 @@ sub default {
 Return the value of the first item that does have a non-zero value.
 
 =cut
+
 sub get_first {
     my (@keys) = @_;
     foreach (@keys) {
@@ -202,16 +219,13 @@ sub get_first {
     return "";
 }
 
-#Helper function, returns the home directory
-#This is usually defined as the environment variable HOME on Unix like
-#systems, and HOMEDRIVE + HOMEPATH on Windows
-
 =item C<< GT::Conf::=_get_home_path() >>
 
 Helper function, returns the home directory environment variable HOME on Unix
 or on windows the environment variables HOMEDRIVE . HOMEPATH
 
 =cut
+
 sub _get_home_path {
     my $homedir = '';
     if (defined($ENV{'HOME'})) {
@@ -224,9 +238,57 @@ sub _get_home_path {
     return $homedir;
 }
 
+=item C<< GT::Conf::conf_dump( [ "regex" ] ) >>
+
+Helper function, writes the entire configure key=value pairs on stderr.
+code example: GT::Conf::conf_dump;
+
+pass a perl regex string to filter the output
+
+=cut
+
+sub conf_dump {
+    my ( $regex ) = @_;
+
+    $regex = ".*" unless $regex;
+    print STDERR "gt configuration file keys and values filtered using \"$regex\"\n";
+    printf STDERR "%s\t%-36s\t%-s\n", "item", "key", "value";
+    my $i = 0;
+    foreach ( sort keys %conf ) {
+        ( grep m/$regex/i, $_ )
+         ? printf STDERR "%3d\t%-36s\t%-s\n", $i, $_, $conf{$_}
+         : ();
+        ++$i;
+    }
+    print STDERR "\n\n";
+}
+
 =pod
 
 =back
 
 =cut
+
+=item C<< my $gt_root_dir = GT::Conf::get_gt_root() >>
+
+Helper function, returns the gt root directory
+which is the directory that contains GT and Scripts
+directories, along with any others that may be there.
+if that configuration key-value is unset check for the
+environment variable GT_ROOT otherwise returns an empty string
+
+=cut
+
+sub get_gt_root {
+    my $gtroot = get( 'GT::Root' );
+    if ( $gtroot ) {
+        return $gtroot;
+    } elsif ( defined $ENV{'GT_ROOT'} ) {
+        return $ENV{'GT_ROOT'};
+    } else {
+        return "";
+    }
+}
+
+
 1;
