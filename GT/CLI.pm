3310d7803ef57ba018038aaf2724c72339218f8d
Chia-liang Kao <clkao@clkao.org>
2009-09-29 23:47:05 +0800

    CLI helper.

---
+++ GT/CLI.pm
@@ -0,0 +1,41 @@
+package GT::CLI;
+
+use GT::DateTime;
+use GT::Eval;
+use GT::Tools qw(:conf :timeframe);
+use GT::Calculator;
+use GT::Conf;
+use Getopt::Long;
+
+sub init {
+    my @getopt_extra = @_;
+
+    GT::Conf::load();
+
+    my $nb_item = GT::Conf::get('Option::nb-item');
+    $nb_item = (defined($nb_item))?$nb_item:200;
+    my ($full, $start, $end, $timeframe, $max_loaded_items) =
+        (0, '', '', 'day', -1);
+    my $man = 0;
+    my @options;
+    Getopt::Long::Configure("require_order");
+    GetOptions('full!' => \$full, 'nb-item=i' => \$nb_item, 
+               "start=s" => \$start, "end=s" => \$end, 
+               "max-loaded-items=s" => \$max_loaded_items,
+               "timeframe=s" => \$timeframe,
+               "option=s" => \@options, "help!" => \$man);
+
+    $timeframe = GT::DateTime::name_to_timeframe($timeframe);
+
+    check_dates($timeframe, $start, $end);
+
+    my $db = create_db_object();
+
+    return sub {
+        my $code = shift;
+        find_calculator($db, $code, $timeframe, $full, $start, $end, $nb_item, $max_loaded_items);
+    }
+
+}
+
+1;
